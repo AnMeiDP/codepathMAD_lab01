9d2c87a03bfc92801027d56eeb464a4243d32cbe
AnMeiDP <93233920+AnMeiDP@users.noreply.github.com>
2022-03-24 22:48:20 -0700

    lab 03

--- app/src/main/java/com/example/anmeisflashcardapp/AddCardActivity.java
+++ app/src/main/java/com/example/anmeisflashcardapp/AddCardActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -24,13 +26,20 @@ public class AddCardActivity extends AppCompatActivity {
         findViewById(R.id.save_card_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent data = new Intent();
                 String inputQuestion = ((EditText) findViewById(R.id.flashcard_question_edittext)).getText().toString();
                 String inputAnswer = ((EditText) findViewById(R.id.flashcard_answer_edittext)).getText().toString();
-                data.putExtra("QUESTION_KEY", inputQuestion);
-                data.putExtra("ANSWER_KEY", inputAnswer);
-                setResult(RESULT_OK, data);
-                finish();
+
+                if (inputQuestion.matches("") || inputAnswer.matches("")) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Must answer both question and answer!", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    toast.show();
+                } else {
+                    Intent data = new Intent();
+                    data.putExtra("QUESTION_KEY", inputQuestion);
+                    data.putExtra("ANSWER_KEY", inputAnswer);
+                    setResult(RESULT_OK, data);
+                    finish();
+                }
             }
         });
     }
--- app/src/main/java/com/example/anmeisflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/anmeisflashcardapp/MainActivity.java
@@ -4,15 +4,27 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.content.Intent;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView flashcardQuestion;
     TextView flashcardAnswer;
+
+    // must be a global variable because we need to access it in both classes in MA
+    FlashcardDatabase flashcardDatabase;
+    // list variable of all flashcards, so that we can flip through them?
+    List<Flashcard> allFlashcards;
+    int cardIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,8 +88,66 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//      implementing toggle functionality, create an object
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        // ensures that you get the most up to date list of flashcards
+        allFlashcards = flashcardDatabase.getAllCards();
 
+        // this code assumes that there are flashcards in the database, so we need to check if database empty
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            Flashcard firstCard = allFlashcards.get(0);
+            flashcardQuestion.setText(firstCard.getQuestion());
+            flashcardAnswer.setText(firstCard.getAnswer());
+        }
+
+        // implementing next button functionality
+        findViewById(R.id.next_card_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (allFlashcards == null || allFlashcards.size() == 0) {
+                    return;
+                }
+
+                cardIndex += 1;
+
+                if (cardIndex >= allFlashcards.size()) {
+                    Snackbar.make(view,
+                            "Last flashcard reached! Returning to the start.",
+                            Snackbar.LENGTH_SHORT)
+                            .show();
+                    cardIndex = 0; // resets index
+                }
+
+                Flashcard currentCard = allFlashcards.get(cardIndex);
+                flashcardQuestion.setText(currentCard.getQuestion());
+                flashcardAnswer.setText(currentCard.getAnswer());
+            }
+        });
+
+        findViewById(R.id.delete_card_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (allFlashcards == null || allFlashcards.size() == 0) {
+                    return;
+                }
+
+                flashcardDatabase.deleteCard(((TextView) findViewById(R.id.flashcard_question_textview)).getText().toString());
+
+                allFlashcards = flashcardDatabase.getAllCards();
+
+                if (allFlashcards == null || allFlashcards.size() == 0) {
+                    flashcardQuestion.setText("Flashcard Deck Empty!");
+                    flashcardAnswer.setText("");
+                    Toast toast = Toast.makeText(getApplicationContext(), "Add a card.", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    toast.show();
+                } else {
+                    Flashcard currentCard = allFlashcards.get(cardIndex - 1);
+                    flashcardQuestion.setText(currentCard.getQuestion());
+                    flashcardAnswer.setText(currentCard.getAnswer());
+                }
+            }
+        });
+//      implementing toggle functionality, create an object
     }
 
     @Override
@@ -90,6 +160,12 @@ public class MainActivity extends AppCompatActivity {
               String answerString = data.getExtras().getString("ANSWER_KEY");
               flashcardQuestion.setText(questionString);
               flashcardAnswer.setText(answerString);
+
+              // saving new flashcard to database
+              Flashcard flashcard = new Flashcard(questionString, answerString);
+              flashcardDatabase.insertCard(flashcard);
+
+              allFlashcards = flashcardDatabase.getAllCards();
            }
         }
     }
